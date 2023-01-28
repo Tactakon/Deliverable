7256b408a281c5610e23641420f6c8e73ef7906e
Tactakon <112525688+Tactakon@users.noreply.github.com>
2023-01-28 15:13:47 -0500

    AppRouteLogin

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import flask
 import random
+import os
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 app = flask.Flask(__name__)
 
 @app.route("/")
-def index():
-    return 'Collaborative DJing WebApp!'
+def main():
+    #if not session.get('logged_in'):
+        #return render_template('login.html')
+    #else:
+    return flask.render_template('login.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/login', methods=['POST'])
+def login():
+    if flask.request.form['psw'] == 'password' and flask.request.form['email'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flask.flash('wrong password!')
+        return main()
+app.secret_key = os.urandom(12)
+app.run(debug=True)
