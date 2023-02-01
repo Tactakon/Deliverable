066d56b12b1344f5092c8999e0330b023d002e30
Tactakon <112525688+Tactakon@users.noreply.github.com>
2023-02-01 11:31:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import os
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
 app = flask.Flask(__name__)
-
+#landing page
 @app.route("/")
 def main():
-    #if not session.get('logged_in'):
-        #return render_template('login.html')
+    #if session.get('logged_in'):
+        #header icon change
     #else:
     return flask.render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    return flask.render_template('signup.html')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if flask.request.form['psw'] == 'password' and flask.request.form['email'] == 'admin':
         session['logged_in'] = True
     else:
         flask.flash('wrong password!')
-        return main()
+        return login()
 app.secret_key = os.urandom(12)
 app.run(debug=True)
