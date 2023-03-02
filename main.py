09a42f72d6ea3e07b361dc38cbc3903a05b68d59
Tactakon <112525688+Tactakon@users.noreply.github.com>
2023-03-01 19:06:24 -0500

    took the unnecessary code out from homeheader def

--- main.py
+++ main.py
@@ -111,8 +111,6 @@ def header():
 #homeheader.html
 @app.route('/homeheader')
 def homeheader():
-    if request.method == 'POST':
-        session['logged_in'] = False
     return flask.render_template('homeheader.html')
 
 #landfooter.html
