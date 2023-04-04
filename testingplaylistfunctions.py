2e8b124b52082a86d2718655399f63bfbb907d06
John <jbergen@student.gsu.edu>
2023-04-04 18:51:31 -0400

    updated database testing function so all tests pass

--- testingplaylistfunctions.py
+++ testingplaylistfunctions.py
@@ -35,32 +35,37 @@ class TestingPlaylistSongRemoveFunction(unittest.TestCase):
             {
                 "songID": 1,
                 "songResult": "Song 1",
-                "artistResult": "Artist 1"
+                "artistResult": "Artist 1",
+                "imageURL": "Image URL 1"
             },
             {
                 "songID": 2,
                 "songResult": "Song 2",
-                "artistResult": "Artist 2"
+                "artistResult": "Artist 2",
+                "imageURL": "Image URL 2"
             },
             {
                 "songID": 3,
                 "songResult": "Song 3",
-                "artistResult": "Artist 3"
+                "artistResult": "Artist 3",
+                "imageURL": "Image URL 3"
             },
             {
                 "songID": 4,
                 "songResult": "Song 4",
-                "artistResult": "Artist 4"
+                "artistResult": "Artist 4",
+                "imageURL": "Image URL 4"
             }
         ]'''
 
         songID = 2
         songResult = "Song 2"
         artistResult = "Artist 2"
+        imageURL = "Image URL 2"
 
         # Call the function to be tested
         updated_songs = RemoveSongFromPlaylist(
-            songs, songID, songResult, artistResult)
+            songs, songID, songResult, artistResult, imageURL)
 
         # Convert the updated songs back to a list
         updated_songs_list = json.loads(updated_songs)
@@ -68,15 +73,15 @@ class TestingPlaylistSongRemoveFunction(unittest.TestCase):
         # Check that the song with songID 2 was removed from the playlist
         self.assertEqual(len(updated_songs_list), 3)
         self.assertNotIn({"songID": 2, "songResult": "Song 2",
-                         "artistResult": "Artist 2"}, updated_songs_list)
+                         "artistResult": "Artist 2", "imageURL": "Image URL 2"}, updated_songs_list)
 
         # Check that the remaining songs are still in the playlist
         self.assertIn({"songID": 1, "songResult": "Song 1",
-                      "artistResult": "Artist 1"}, updated_songs_list)
+                      "artistResult": "Artist 1", "imageURL": "Image URL 1"}, updated_songs_list)
         self.assertIn({"songID": 3, "songResult": "Song 3",
-                      "artistResult": "Artist 3"}, updated_songs_list)
+                      "artistResult": "Artist 3", "imageURL": "Image URL 3"}, updated_songs_list)
         self.assertIn({"songID": 4, "songResult": "Song 4",
-                      "artistResult": "Artist 4"}, updated_songs_list)
+                      "artistResult": "Artist 4", "imageURL": "Image URL 4"}, updated_songs_list)
 
 class TestingAddSongtoPlaylistFunction(unittest.TestCase):
     """
@@ -91,32 +96,37 @@ class TestingAddSongtoPlaylistFunction(unittest.TestCase):
             {
                 "songID": 1,
                 "songResult": "Song 1",
-                "artistResult": "Artist 1"
+                "artistResult": "Artist 1",
+                "imageURL": "Image URL 1"
             },
             {
                 "songID": 2,
                 "songResult": "Song 2",
-                "artistResult": "Artist 2"
+                "artistResult": "Artist 2",
+                "imageURL": "Image URL 2"
             },
             {
                 "songID": 3,
                 "songResult": "Song 3",
-                "artistResult": "Artist 3"
+                "artistResult": "Artist 3",
+                "imageURL": "Image URL 3"
             },
             {
                 "songID": 4,
                 "songResult": "Song 4",
-                "artistResult": "Artist 4"
+                "artistResult": "Artist 4",
+                "imageURL": "Image URL 4"
             }
         ]'''
 
         songID = 5
         songResult = "Song 5"
         artistResult = "Artist 5"
+        imageURL = "Image URL 5"
 
         # Call the function to be tested
         updated_songs = AddSongtoPlaylist(
-            songs, songID, songResult, artistResult)
+            songs, songID, songResult, artistResult, imageURL)
 
         # Convert the updated songs back to a list
         updated_songs_list = json.loads(updated_songs)
@@ -124,17 +134,17 @@ class TestingAddSongtoPlaylistFunction(unittest.TestCase):
         # Check that the song with songID 2 was added to the playlist
         self.assertEqual(len(updated_songs_list), 5)
         self.assertIn({"songID": 5, "songResult": "Song 5",
-                         "artistResult": "Artist 5"}, updated_songs_list)
+                         "artistResult": "Artist 5", "imageURL": "Image URL 5"}, updated_songs_list)
 
         # Check that the remaining songs are still in the playlist
         self.assertIn({"songID": 1, "songResult": "Song 1",
-                      "artistResult": "Artist 1"}, updated_songs_list)
+                      "artistResult": "Artist 1", "imageURL": "Image URL 1"}, updated_songs_list)
         self.assertIn({"songID": 2, "songResult": "Song 2",
-                      "artistResult": "Artist 2"}, updated_songs_list)
+                      "artistResult": "Artist 2", "imageURL": "Image URL 2"}, updated_songs_list)
         self.assertIn({"songID": 3, "songResult": "Song 3",
-                      "artistResult": "Artist 3"}, updated_songs_list)
+                      "artistResult": "Artist 3", "imageURL": "Image URL 3"}, updated_songs_list)
         self.assertIn({"songID": 4, "songResult": "Song 4",
-                      "artistResult": "Artist 4"}, updated_songs_list)
+                      "artistResult": "Artist 4", "imageURL": "Image URL 4"}, updated_songs_list)
 
 if __name__ == '__main__':
     unittest.main()
